8fa48bd6ff13e52c8df6e9c4552c9c445cea41c6
shaxzod22 <shaxzod01901@gmail.com>
2023-11-24 23:47:07 +0500

    lesson 3

---
+++ src/components/contact/User.jsx
@@ -0,0 +1,33 @@
+
+import PropTypes from 'prop-types';
+
+import './contact.css'
+
+const User = (props) => {
+const {firstname,lastname,phoneNumber,relativeness,id,deleteI}=props
+const classOfRelativeness = relativeness.toLowerCase()=='family'?'family relativeness':relativeness.toLowerCase()=='friends'?'friend relativeness':relativeness.toLowerCase()=='relatives'?'relative relativeness':relativeness.toLowerCase()=='other'?'other relativeness':'relativeness'
+
+
+  return (
+    <>
+    <div className='user__item'>
+     <div className="user__info__box">
+     <h3 className='user__name'>{firstname} {lastname} <span className={classOfRelativeness}>{relativeness}</span></h3>
+      <p className="phone__number">{phoneNumber}</p>
+     </div>
+     <button onClick={()=>deleteI(id)} className="delete">Delete</button>
+    </div>
+    </>
+  )
+}
+
+User.propTypes={
+firstname:PropTypes.string,
+lastname:PropTypes.string,
+phoneNumber:PropTypes.string,
+relativeness:PropTypes.string,
+id:PropTypes.number,
+deleteI:PropTypes.func,
+}
+
+export default User
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,92 @@
+import { Fragment } from "react"
+import { useRef } from "react"
+import { useState } from "react"
+import User from "../components/contact/User"
+
+const Contact = () => {
+    
+    const [users,setusers]= useState(JSON.parse(localStorage.getItem('users'))||[])
+    let firstNameInputRef = useRef()
+    let lastNameInputRef = useRef()
+    let phoneInputRef = useRef()
+    let selectionRef = useRef()
+   
+    function submit(e){
+        e.preventDefault()
+        console.log(firstNameInputRef.current.value.trim());
+        let firstname = firstNameInputRef.current.value.trim()
+        let lastname = lastNameInputRef.current.value.trim()
+        let phoneNumber = phoneInputRef.current.value.trim()
+        if(firstname && phoneNumber){
+            const newUser = {
+                id:new Date(),
+                firstname,
+                lastname,
+                phoneNumber,
+                relativeness:selectionRef.current.value,
+            }
+            let newData = [...users,newUser]
+            localStorage.setItem('users',JSON.stringify(newData))
+            setusers(newData)
+        }else{
+            alert('Enter both firstname and phone number')
+        }
+        
+      
+        e.target.reset()
+    }
+    
+    function deleteI(id) {
+        console.log(id);
+        let newData = users.filter((el)=>el.id != id )
+        localStorage.setItem('users',JSON.stringify(newData))
+        setusers(newData)
+    }
+    let renderData = users.map((user,i)=>(
+        <User key={i} {...user} deleteI={deleteI}/>
+        ))
+
+        
+        return (
+            <Fragment>
+            <div className="container">
+            <form onSubmit={submit} className="form">
+            <label className="label">
+            First name
+            <input type="text" ref={firstNameInputRef} className="firstname input" placeholder="First name" />
+            </label>
+            <label className="label">
+            Last name (Optional)
+            <input type="text" ref={lastNameInputRef} className="lastname input" placeholder="Last name" />
+            </label>
+            <label className="label">
+            Phone number
+            <input ref={phoneInputRef} type="text" className="phone input" placeholder="Phone number" />
+            </label>
+            <label className="label">
+            Choose relationship
+            <select ref={selectionRef} defaultValue='none'  className="input selection">
+            <option value="none"></option>
+            <option value="family">Family</option>
+            <option value="friends">Friends</option>
+            <option value="relatives">Relatives</option>
+            <option value="other">Other</option>
+            </select>
+            </label>
+            <button className="add__btn">Add contact</button>
+            </form>
+            
+            <ul className="navbar__list">
+            <li className="navbar__item">All</li>
+            <li className="navbar__item navbar__item-active">Favourites</li>
+            </ul>
+            <div className="users__list">
+            {renderData}
+            </div>
+            </div>
+            </Fragment>
+            )
+        }
+        
+        export default Contact
+        
