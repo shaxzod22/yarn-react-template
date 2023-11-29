e060e409fcbbb6836ec9e1066b461da47db51471
shaxzod22 <shaxzod01901@gmail.com>
2023-11-29 23:21:19 +0500

    s

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,42 @@
-import axios from "axios"
 import { useEffect } from "react"
 import { useState } from "react"
+
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 const HomePage = () => {
     const urlAll = `https://api.spaceflightnewsapi.net/v3/articles`
-const [info,setInfo] = useState([])
-    async function getData(url){
-        try{
-            const {data} = await axios(url)
-            setInfo(data)
-        }catch{
-            console.log('a');
-        }
-    }
-
+    const [info,setInfo] = useState([])
+    
+    
     useEffect(()=>{
-getData(urlAll)
-
-    },[])
- 
-  return (
-    <div className="container">
-      <h1 className="heading">News</h1>
-
-      <ul className="news__list">
+        async function getData(url){
+            try{
+                const res = await axios(url)
+              
+                setInfo(res.data)
+            }catch{
+                console.log('a');
+            }
+        }
+        getData(urlAll)
+        
+    },[urlAll])
+    console.log(info);
+    return (
+        <div className="container">
+        <h1 className="heading">News</h1>
+        
+        <ul className="news__list">
         {info.map((el,index)=>(
             <li key={index} className="news__item">
-                <Link to='/'>a</Link>
+            <Link to={`/${index+1}`}>a</Link>
             </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-export default HomePage
+            ))}
+            </ul>
+            </div>
+            )
+        }
+        
+        export default HomePage
+        
